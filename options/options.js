d7c89161c1120fba681c0791bb978faa4d23265f
Nick VanSimaeys <nickvansimaeys@gmail.com>
2023-02-24 13:22:51 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "falling-pond-39102",
   url: "https://falling-pond-39102.botics.co",
   api: "https://falling-pond-39102.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
